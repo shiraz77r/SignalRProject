060fca796a01f0c0fe097f7e5bf34aed9d9a77e7
shiraz77r <sheraz.mehfooz@scorpbit.com>
2021-08-19 11:51:53 +0500

    Message from Client to Server
    Message from Client to Server

--- Hubs/NotificationsHub.cs
+++ Hubs/NotificationsHub.cs
@@ -8,6 +8,9 @@ namespace SignalRProject.Hubs
 {
     public class NotificationsHub : Hub
     {
-
+        public async Task BroadcastFromClient(string message) 
+        {
+            await Clients.All.SendAsync("Broadcast", message);
+        }
     }
 }
